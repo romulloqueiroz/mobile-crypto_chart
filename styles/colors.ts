9c64ba901a1157efdeb395b7815207a03bb40a9d
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-01-22 22:29:01 +0900

    styling

---
+++ styles/colors.ts
@@ -0,0 +1,14 @@
+
+   
+export type ColorType = keyof typeof color
+
+export const color = {
+  white: '#ffffff',
+  black: '#000000',
+  sasquatch: '#FF507c',
+  ultramarineBlue: '#5257F2',
+  aquamarine: '#6BEFD0',
+  violet: '#EE8FFA',
+  topaz: '#FFBF68',
+  vividTangelo: '#F26D2B'
+}
