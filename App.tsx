3b968a4d93a76d058b6a12f09c1b8f70bb5ab06f
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-01-23 14:35:34 +0900

    restructuring

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
 import styled from 'styled-components/native'
 import { StatusBar } from 'expo-status-bar'
-import CandleChartProvider from './contexts/CandleChartContext'
 import CandleChartScreen from './screens/CandleChartScreen'
 
 
-const App = () => {
-  return (
-    <Container>
-
-      <CandleChartProvider>
-        <CandleChartScreen />
-      </CandleChartProvider>
-
-      <StatusBar style='auto' />
-    </Container>
-  )
-}
+const App = () => (
+  <Container>
+    <CandleChartScreen />
+    <StatusBar style='auto' />
+  </Container>
+)
 
 const Container = styled.View`
   flex: 1;
--- screens/CandleChartScreen.tsx
+++ screens/CandleChartScreen.tsx
@@ -2,15 +2,20 @@ import styled from 'styled-components/native'
 import { ImageBackground } from 'react-native'
 import { CandleChart } from '../components'
 import Values from '../Values'
+import CandleChartProvider from '../contexts/CandleChartContext'
 
-const CandleChartScreen = () => (
-  <BackgroundImage source={require('../assets/back.png')}>
-    <CandleChart />
-    <View pointerEvents='none'>
-      <Values />
-    </View>
-  </BackgroundImage>
-)
+const CandleChartScreen = () => {
+  return (
+    <CandleChartProvider>
+      <BackgroundImage source={require('../assets/back.png')}>
+        <CandleChart />
+        <View pointerEvents='none'>
+          <Values />
+        </View>
+      </BackgroundImage>
+    </CandleChartProvider>
+  )
+}
 
 const BackgroundImage = styled(ImageBackground)`
   flex: 1;
